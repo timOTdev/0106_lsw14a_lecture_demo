0b6646bab8495b799f8ccf9df0fd7c834248bfaa
Timothy Hoang <timh1203@gmail.com>
2018-10-25 15:41:29 -0400

    Add tests for POST route

--- index.test.js
+++ index.test.js
@@ -2,10 +2,17 @@ const request = require('supertest')
 const server = require('./server.js');
 
 describe('server', () => {
+  it('can run tests', () => {
+    expect(true).toBeTruthy();
+  })
+  it('can run more tests', () => {
+    expect(false).toBeFalsy();
+  })
+
   describe('GET /', () => {
     it('returns status code 200(OK)', async () => {
-      const response = await request(server).get('/')
-      expect(response.status).toBe(200)
+      let response = await request(server).get('/')
+      expect(response.status).toBe(200);
 
       // return request(server)
       //   .get('/')
@@ -17,14 +24,29 @@ describe('server', () => {
     })
     it('returns correct content', async () => {
       const response = await request(server).get('/');
-      expect(response.body).toEqual({ Message: 'Good to go!' });
+      expect(response.body).toEqual({ message: 'Good to go!' });
     })
   })
 
-  it('can run tests', () => {
-    expect(true).toBeTruthy();
-  })
-  it('can run more tests', () => {
-    expect(false).toBeFalsy();
+  describe('POST /hello/:name', () => {
+    it('returns status code 201(Created)', async () => {
+      const response = await request(server).post(`/hello/john`);
+      expect(response.status).toBe(201);
+    })
+    it('returns JSON', async () => {
+      const response = await request(server).post(`/hello/john`);
+      expect(response.type).toBe('application/json');
+    })
+    it('should greet the person', async () => {
+      const name = 'Patrick';
+      const lastName = 'Thompson';
+      const response = await request(server).post(`/hello/${name}`).send({ lastName })
+      expect(response.body).toEqual({ hello: 'Patrick Thompson' });
+    })
+    it('adds person to the Doe family if no last name provided', async () => {
+      const name = 'Adam';
+      const response = await request(server).post(`/hello/${name}`)
+      expect(response.body).toEqual({ hello: 'Adam Doe' });
+    })
   })
 })
--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const port = 9000;
 server.use(express.json());
 
 server.route('/')
-  .get((req, res) => res.status(200).json({ Message: 'Good to go!' }))
+  .get((req, res) => res.status(200).json({ message: 'Good to go!' }))
 
+server.route('/hello/:name')
+  .post((req, res) => {
+    const { name } = req.params;
+    const lastName = req.body.lastName || 'Doe';
+    res.status(201).json({ hello: `${name} ${lastName}` })
+  })
 module.exports = server;
